0ce6606273888c10360b2421481964d067f8a1d6
Vasileios Iliopoulos <109439587+iliopou@users.noreply.github.com>
2022-08-06 12:30:09 +0200

    Add files via upload
    Faster inner loops in partition function

---
+++ qsort4_tco.py
@@ -0,0 +1,44 @@
+import random
+from statistics import median
+import array as arr 
+
+A = arr.array('i')                                                         # i - integers in interval [-2^31, 2^31]
+
+def insertion_sort(A):    
+ for j in range(1,len(A)):
+     key  = A[j];
+     i = j - 1;
+     while (i >= 0 and A[i] > key):            
+      A[i + 1] = A[i]
+      i -= 1 
+     A[i + 1] = key 
+
+def Hoare_partition(A, lo, hi):
+  pivot = median(random.sample(A, 3)); 
+  i = lo - 1; j = hi + 1; 
+  while True:
+    while True:
+      i += 1
+      if (A[i] >= pivot):
+        break
+    while True:
+      j -= 1
+      if (A[j] <= pivot):
+        break
+    if (i >= j):
+      return j
+    else:
+      A[i], A[j] = A[j], A[i];
+    
+  
+def qsort(A, lo, hi):
+ while (lo + 8 < hi):                                                        # Arrays with at most 9 elements are sorted by insertion sort
+  q = Hoare_partition(A, lo, hi);
+  if q - lo < hi - q:                                                        # Recur on smaller subarray and loop on larger - at most log(n) stack frames
+    qsort(A, lo, q-1); 
+    lo = q + 1
+  else:
+   qsort(A, q+1, hi);
+   hi = q - 1;
+ else:
+  insertion_sort(A)
